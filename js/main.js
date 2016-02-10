e1536bcbb11610852622b549879a18723587229c
Monorom <monoroms@hotmail.com>
2016-02-10 08:39:07 +0100

    First push

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        direction: 'vertical',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30,
+        mousewheelControl: true
+    })
+});
